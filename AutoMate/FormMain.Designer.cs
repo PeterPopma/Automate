1da38c180efcfc73b00a3f420fb7e54b980f31d1
Peter Popma <p_popma@hotmail.com>
2021-04-24 12:15:08 +0200

    Initial version

---
+++ AutoMate/FormMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace AutoMate
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMoveMouse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMoveMouse
+            // 
+            this.buttonMoveMouse.Location = new System.Drawing.Point(272, 125);
+            this.buttonMoveMouse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonMoveMouse.Name = "buttonMoveMouse";
+            this.buttonMoveMouse.Size = new System.Drawing.Size(228, 111);
+            this.buttonMoveMouse.TabIndex = 0;
+            this.buttonMoveMouse.Text = "Move Mouse";
+            this.buttonMoveMouse.UseVisualStyleBackColor = true;
+            this.buttonMoveMouse.Click += new System.EventHandler(this.buttonMoveMouse_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.buttonMoveMouse);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMoveMouse;
+    }
+}
+
---
+++ AutoMate/FormMain.cs
@@ -0,0 +1,93 @@
+﻿using Hotkeys;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoMate
+{
+    public partial class FormMain : Form
+    {
+        private static System.Windows.Forms.Timer playbackTimer;
+        static int counterMouseTime;
+        static int xMouse, yMouse, xOffset, yOffset;
+        static double xPos, yPos, angle;
+        const int CIRCLE_RADIUS = 100;
+        const int MOUSE_STEPS = 50;
+        private Hotkeys.GlobalHotkey globalHotKeys;
+
+        // Specify what you want to happen when the Elapsed event is raised.
+        private static void OnTimedEventUpdatePlayback(object sender, EventArgs eArgs)
+        {
+            if (counterMouseTime > 0)
+            {
+                counterMouseTime--;
+                angle = ((MOUSE_STEPS - counterMouseTime) / Convert.ToDouble(MOUSE_STEPS)) * 2 * Math.PI;
+                xPos = Math.Cos(angle) * CIRCLE_RADIUS;
+                yPos = Math.Sin(angle) * CIRCLE_RADIUS;
+            }
+            int xMouseNew = Convert.ToInt32(xPos);
+            int yMouseNew = Convert.ToInt32(yPos);
+            if (!xMouseNew.Equals(xMouse) || !yMouseNew.Equals(yMouse))
+            {
+                xMouse = xMouseNew;
+                yMouse = yMouseNew;
+                Cursor.Position = new Point(xMouse + xOffset, yMouse + yOffset);
+            }
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!globalHotKeys.Unregiser())
+                MessageBox.Show("Hotkeys failed to unregister!");
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            if (!globalHotKeys.Register())
+                MessageBox.Show("Hotkeys failed to register!");
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            globalHotKeys = new Hotkeys.GlobalHotkey(Constants.NOMOD, Keys.O, this);
+            SetupTimers();
+        }
+
+        private void HandleHotkey()
+        {
+            xOffset = Cursor.Position.X - CIRCLE_RADIUS;
+            yOffset = Cursor.Position.Y;
+            counterMouseTime = MOUSE_STEPS;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void SetupTimers()
+        {
+            // Create a timer for screen updates.
+            playbackTimer = new System.Windows.Forms.Timer();
+            playbackTimer.Interval = 1;
+            playbackTimer.Tick += new EventHandler(OnTimedEventUpdatePlayback);
+            playbackTimer.Start();
+        }
+
+        private void buttonMoveMouse_Click(object sender, EventArgs e)
+        {
+            xOffset = Cursor.Position.X - CIRCLE_RADIUS;
+            yOffset = Cursor.Position.Y;
+            counterMouseTime = MOUSE_STEPS;
+        }
+    }
+}
